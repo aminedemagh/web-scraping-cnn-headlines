dd2b0218ae518aebdd14cb613d5c0f7cccfd9d2e
Demagh Mohamed El Amine <d-amine@hotmail.fr>
2020-12-26 21:52:49 +0100

    feat : Filter out li tags without text

--- cnn_scrapper.py
+++ cnn_scrapper.py
@@ -9,7 +9,8 @@ section1 = driver.find_element_by_id("intl_homepage1-zone-1")
 lis = section1.find_elements_by_xpath(".//child::li")
 hrefs = []
 #print(lis.text)
-for li in lis:    
+for li in lis:
+    if li.text == '': continue    
     atags = li.find_elements_by_xpath(".//child::a")
     headlines = []
     for a in atags:
