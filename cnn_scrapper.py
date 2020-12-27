f443ef48aa16a903e9aed048c7484987b6f5efbf
Demagh Mohamed El Amine <d-amine@hotmail.fr>
2020-12-28 00:04:51 +0100

    refactor : Wrap headline scrapping in a function

--- cnn_scrapper.py
+++ cnn_scrapper.py
@@ -7,23 +7,35 @@ def getWebDriver():
     driver.set_window_size(1120, 550)
     return driver
 
-driver = getWebDriver()
-driver.get('https://edition.cnn.com')
-# Fetch the first section of the cnn website
-section1 = driver.find_element_by_id("intl_homepage1-zone-1")
-lis = section1.find_elements_by_xpath(".//child::li")
-hrefs = []
-#print(lis.text)
-for li in lis:
-    if li.text == '': continue    
-    atags = li.find_elements_by_xpath(".//child::a")
-    headlines = []
-    for a in atags:
-        title = a.text
-        if title == "": continue
-        href = a.get_attribute('href')
-        headlines.append((title, href))
-    print(li.text + "\n ------------------------------------\n" + str(headlines)
-    +"\n _______________________________________________________________ \n")
+def getHeadlines(webdriver):
+    webdriver.get('https://edition.cnn.com')
+    # Fetch the first section of the cnn website
+    # id = intl_homepage1-zone-1
+    section1 = webdriver.find_element_by_id("intl_homepage1-zone-1")
+    # Titles are listed in li tags under section1
+    lis = section1.find_elements_by_xpath(".//child::li")
+    # Each li tag contains a set of a tags
+    # Some a tags contain images and others text 
+    # This app is only interested in text and their corresponding href
+    for li in lis:
+        # Ignore the iteration if the li tags doesn't contain any text
+        if li.text == '' : continue    
+        atags = li.find_elements_by_xpath(".//child::a")
+        headlines = []
+        for a in atags:
+            # Extract the titles
+            title = a.text
+            # Ignore the iteration if there's no text in it
+            if title == "" : continue
+            # Extract the corresponding href
+            href = a.get_attribute('href')
+            headlines.append((title, href))
+        print(li.text + "\n ------------------------------------\n" + str(headlines)
+        +"\n _______________________________________________________________ \n")
 
-#intl_homepage1-zone-1
+driver = getWebDriver()
+getHeadlines(driver)
+
+
+
+
